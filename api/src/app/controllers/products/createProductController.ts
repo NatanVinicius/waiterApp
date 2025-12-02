8f2e10598719671f443a1335917252833d79d062
Natan de Paula <natanviniciusdepaula@gmail.com>
2025-12-02 02:47:07 -0300

    feat(product): added routes for Product

---
+++ api/src/app/controllers/products/createProductController.ts
@@ -0,0 +1,19 @@
+import type { Request, Response } from 'express';
+import { ProductSchema } from '../../../schemas/product.schema.js';
+import { createProductssService } from '../../services/products/createProductsService.js';
+import { ZodError } from 'zod';
+
+export const createProductController = async (req: Request, res: Response) => {
+  try {
+    const data = ProductSchema.parse(req.body);
+    const product = await createProductssService(data);
+
+    res.status(201).json(product);
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return res.status(400).json({ errors: error.issues });
+    }
+
+    return res.status(500).json({ error: error instanceof Error ? { message: error.message } : error });
+  }
+};
