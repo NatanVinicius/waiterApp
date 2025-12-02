e1b5f550fb0f9df0a9e4f961054e095970e3d30d
Natan de Paula <natanviniciusdepaula@gmail.com>
2025-12-02 02:38:51 -0300

    feat: add Category schema with Zod validation

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,15 +1,18 @@
-import express from "express";
-import mongoose from "mongoose";
+import express from 'express';
+import mongoose from 'mongoose';
+import { router } from './router.js';
 
 mongoose
-  .connect("mongodb://localhost:27017")
+  .connect('mongodb://localhost:27017')
   .then(() => {
-    console.log("Conectado ao mongo");
+    console.log('Conectado ao mongo');
 
     const app = express();
     const port = 3001;
     app.listen(port, () => {
       console.log(`Server is running at http://localhost:${port}`);
+      app.use(express.json());
+      app.use('/', router);
     });
   })
-  .catch(() => console.log("Erro ao conectar ao mongo"));
+  .catch(() => console.log('Erro ao conectar ao mongo'));
