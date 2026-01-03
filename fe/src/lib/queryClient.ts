a73de8fd6bfac11b6b3d5c0658f79a28812455c5
Natan de Paula <natanviniciusdepaula@gmail.com>
2026-01-03 12:35:09 -0300

    installed react-query and config.

---
+++ fe/src/lib/queryClient.ts
@@ -0,0 +1,10 @@
+import { QueryClient } from "@tanstack/react-query";
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 100 * 60 * 1,
+      retry: 1,
+    },
+  },
+});
